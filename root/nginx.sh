c5163d2618b31f2404212f230bb716ee3ae1ce67
WiggidyW <36451315+WiggidyW@users.noreply.github.com>
2019-08-13 09:48:19 -0500

    Update nginx.sh

--- root/nginx.sh
+++ root/nginx.sh
@@ -1,4 +1,4 @@
 #!/bin/ash
-echo "server {listen ${PORT}; location / {proxy_pass http://${IP}:${PORT};}}" \
+echo "server {listen 8080; location / {proxy_pass http://${IP}:${PORT};}}" \
 > /etc/nginx/conf.d/loadbalance-storage-controller.conf
 nginx -s reload
