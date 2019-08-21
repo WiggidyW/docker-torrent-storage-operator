57725999ed7ac2079f1942dce77e9f98a9a93634
WiggidyW <36451315+WiggidyW@users.noreply.github.com>
2019-08-21 13:26:59 -0500

    Update rsync.sh

--- root/rsync.sh
+++ root/rsync.sh
@@ -1,3 +1,3 @@
 #!/bin/ash
-rsync --remove-source-files -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /keys/${KEY}" -arv /downloads/ ${IP}:${TARGET_DOWNLOADS_PATH}
-rsync --remove-source-files -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /keys/${KEY}" -arv /torrents/ ${IP}:${TARGET_TORRENTS_PATH}
+rsync --remove-source-files -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /keys/${SSH_KEY} -p ${STORAGE_SSH_PORT}" -arv /downloads/ ${IP}:${STORAGE_DOWNLOADS_PATH}
+rsync --remove-source-files -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /keys/${SSH_KEY} -p ${STORAGE_SSH_PORT}" -arv /torrents/ ${IP}:${STORAGE_TORRENTS_PATH}
