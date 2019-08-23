2278570b50c49ca8613a99ed1f2c743498a2f18c
WiggidyW <36451315+WiggidyW@users.noreply.github.com>
2019-08-23 00:25:26 -0500

    Update rsync.sh

--- root/rsync.sh
+++ root/rsync.sh
@@ -1,5 +1,6 @@
 #!/bin/ash
 IP=$1
 if [ -e /downloads/*.${LOCK_FILE} ] || [ -e /torrents/*.${LOCK_FILE} ]; then echo "error: files in use" && exit; fi;
-rsync --remove-source-files -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /keys/${SSH_KEY} -p ${STORAGE_SSH_PORT}" -arv /downloads/ $IP:${STORAGE_DOWNLOADS_PATH}
-rsync --remove-source-files -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /keys/${SSH_KEY} -p ${STORAGE_SSH_PORT}" -arv /torrents/ $IP:${STORAGE_TORRENTS_PATH}
+ls /downloads > /downloads_list.txt && ls /torrents > /torrents_list.txt
+rsync --remove-source-files -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /keys/${SSH_KEY} -p ${STORAGE_SSH_PORT}" -arv --files-from=/downloads_list.txt /downloads $IP:${STORAGE_DOWNLOADS_PATH}
+rsync --remove-source-files -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /keys/${SSH_KEY} -p ${STORAGE_SSH_PORT}" -arv --files-from=/torrents_list.txt /torrents $IP:${STORAGE_TORRENTS_PATH}
