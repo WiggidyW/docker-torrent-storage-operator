62e22ac0410997132248e45e33847edbf347c909
WiggidyW <36451315+WiggidyW@users.noreply.github.com>
2019-08-22 22:48:43 -0500

    Update rsync.sh

--- root/rsync.sh
+++ root/rsync.sh
@@ -1,5 +1,5 @@
 #!/bin/ash
 IP=$1
-if [ -e /downloads/${LOCK_FILE} ] || [ -e /torrents/${LOCK_FILE} ]; then echo "error: files in use" && exit; fi;
+if [ -e /downloads/*.${LOCK_FILE} ] || [ -e /torrents/*.${LOCK_FILE} ]; then echo "error: files in use" && exit; fi;
 rsync --remove-source-files -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /keys/${SSH_KEY} -p ${STORAGE_SSH_PORT}" -arv /downloads/ $IP:${STORAGE_DOWNLOADS_PATH}
 rsync --remove-source-files -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /keys/${SSH_KEY} -p ${STORAGE_SSH_PORT}" -arv /torrents/ $IP:${STORAGE_TORRENTS_PATH}
